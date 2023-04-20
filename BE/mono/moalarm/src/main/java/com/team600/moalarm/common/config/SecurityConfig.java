cb7b6de1d2f71c14f2ce17d9b37ddafdb5f5f338
오민성 <qww1552@naver.com>
2023-04-20 17:13:13 +0900

    Feat: Spring security 설정

---
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/common/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.team600.moalarm.common.config;
+
+import com.team600.moalarm.auth.filter.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    public SecurityFilterChain web(HttpSecurity http) throws Exception {
+        return http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorize -> authorize
+                        .antMatchers("/auth/signin")
+                        .permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
