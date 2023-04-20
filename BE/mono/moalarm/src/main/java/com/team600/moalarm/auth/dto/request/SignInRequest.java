b69929a06c912e506dce43c42b351a43809b7416
오민성 <qww1552@naver.com>
2023-04-20 17:58:21 +0900

    Feat: setter

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/auth/dto/request/SignInRequest.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/auth/dto/request/SignInRequest.java
@@ -1,10 +1,12 @@
 package com.team600.moalarm.auth.dto.request;
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @Getter
+@Setter
 public class SignInRequest {
 
     private String email;
