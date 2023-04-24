a701ae5caaf72f4c53216f670a69e4c6b3eea1fc
오민성 <qww1552@naver.com>
2023-04-24 14:36:42 +0900

    Feat: custom exception 정의

---
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/common/exception/CustomException.java
@@ -0,0 +1,8 @@
+package com.team600.moalarm.common.exception;
+
+public abstract class CustomException extends RuntimeException{
+
+    public CustomException(String message) {
+        super(message);
+    }
+}
