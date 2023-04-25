f676ed5158f2ff9ade76a5cc97ef009a547e00a3
오민성 <qww1552@naver.com>
2023-04-25 23:38:28 +0900

    feat: index 페이지 접근 시 토큰 삭제

--- FE/moalarm/main.js
+++ FE/moalarm/main.js
@@ -1,6 +1,5 @@
 (() => {
   'use strict'
-  window.localStorage.clear();
   document.querySelector('#navbarSideCollapse').addEventListener('click', () => {
     document.querySelector('.offcanvas-collapse').classList.toggle('open')
   })
