d9702af7997a392863f48b1522e64cc6b8309e86
오민성 <qww1552@naver.com>
2023-04-26 20:25:11 +0900

    feat: channel api

--- FE/moalarm/api/http.js
+++ FE/moalarm/api/http.js
@@ -1,24 +1,33 @@
 const baseURL = "http://localhost:8080";
 
-const defaultOption = {
-    headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  }
+const defaultOption = () => {
+    headers = {
+        'Content-type': 'application/json; charset=UTF-8',
+    }
+    const token = localStorage.getItem("token");
+    if (token != null) {
+        headers.Authorization = token;
+    }
+    return headers;
 };
+
 async function get(url) {
-    return await fetch(baseURL + url, defaultOption)
-            .then((response) => response.json());
+    return await fetch(baseURL + url, {
+        method: "GET",
+        headers: defaultOption()
+    }).then((response) => {
+        if (response.status === 204) return;
+        return response.json();
+    }).catch(error => alert(error));
 }
 
 async function post(url, data) {
     return await fetch(baseURL + url, {
         method: "POST",
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-          },
+        headers: defaultOption(),
         body: JSON.stringify(data)
     }).then(response => {
         if (response.status === 204) return;
         return response.json()
-    });
+    }).catch(error => alert(error));
 }
