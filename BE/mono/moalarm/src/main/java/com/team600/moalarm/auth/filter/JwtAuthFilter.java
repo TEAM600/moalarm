367ef1be02907bab4889429178344f9bb2c106a6
Seyoung Goo <0917sy@gmail.com>
2023-04-26 18:07:37 +0900

    refactor: separate interest from TokenManager

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/auth/filter/JwtAuthFilter.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/auth/filter/JwtAuthFilter.java
@@ -1,6 +1,7 @@
 package com.team600.moalarm.auth.filter;
 
-import com.team600.moalarm.auth.service.TokenManager;
+import com.team600.moalarm.auth.service.EncryptJWTManager;
+import com.team600.moalarm.auth.vo.JwtDecryptResult;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -8,8 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,15 +23,23 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String AUTHENTICATION_REQUEST_URL = "/auth";
-    private final TokenManager tokenManager;
+    private final EncryptJWTManager encryptJWTManager;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
         String token = resolveToken(request);
 
-        Authentication authentication = tokenManager.getAuthentication(token);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+        JwtDecryptResult decryptedJwt = encryptJWTManager.decryptJwt(token);
+
+        if (decryptedJwt != null) {
+            User principal = new User(decryptedJwt.getSubject(), "", decryptedJwt.getAuthorities());
+            Authentication authentication = new UsernamePasswordAuthenticationToken(principal,
+                    token,
+                    decryptedJwt.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         filterChain.doFilter(request, response);
     }
 
