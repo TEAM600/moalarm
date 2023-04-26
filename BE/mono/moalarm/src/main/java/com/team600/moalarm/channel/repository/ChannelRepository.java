220e5192fa4ed341e484532595f96f0ec0ec8350
jehyuck <wpgur07@gmail.com>
2023-04-26 13:14:48 +0900

    feat: 공통으로 사용할 레포지토리 생성

---
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/repository/ChannelRepository.java
@@ -0,0 +1,8 @@
+package com.team600.moalarm.channel.repository;
+
+import com.team600.moalarm.channel.entity.Channel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChannelRepository extends JpaRepository<Channel, Long> {
+
+}
--- BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/service/impl/ChannelServiceImpl.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/service/impl/ChannelServiceImpl.java
@@ -1,6 +1,7 @@
 package com.team600.moalarm.channel.service.impl;
 
 import com.team600.moalarm.channel.dto.response.ChannelPossessionDto;
+import com.team600.moalarm.channel.repository.ChannelRepository;
 import com.team600.moalarm.channel.service.ChannelService;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class ChannelServiceImpl implements ChannelService {
+
+    private final ChannelRepository channelRepository;
 
     @Override
     public List<ChannelPossessionDto> getPossessions(String memberId) {
