1f9940e6e10e2873fa1464b2d2fc200d600472b0
96hsjeong <96.hs.jeong@gmail.com>
2023-04-26 14:58:40 +0900

    feat: Member Repository & SignUp 수정

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/member/dto/request/SignUpRequest.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/member/dto/request/SignUpRequest.java
@@ -1,11 +1,10 @@
 package com.team600.moalarm.member.dto.request;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
 
 @ToString(of = {"email"})
 @Getter
@@ -17,12 +16,5 @@ public class SignUpRequest {
 
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자 이상, 영문, 숫자, 특수문자가 모두 포함되어야 합니다.")
     private String password;
-
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$", message = "비밀번호는 최소 8자 이상, 영문, 숫자, 특수문자가 모두 포함되어야 합니다.")
-    private String confirmPassword;
-
-    public boolean isPasswordMatch() {
-        return password.equals(confirmPassword);
-    }
 
 }
--- BE/mono/moalarm/src/main/java/com/team600/moalarm/member/service/MemberService.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.team600.moalarm.member.service;
 
 import com.team600.moalarm.member.dto.request.SignUpRequest;
 import com.team600.moalarm.member.entity.Member;
+import com.team600.moalarm.member.exception.EmailConflictException;
 import com.team600.moalarm.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,17 +16,17 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+
     public void signUp(SignUpRequest signUpRequest) {
-        log.info("[SignUp] 회원가입 시작");
-        if (!signUpRequest.isPasswordMatch()) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        log.info("[SignUp] start");
+        if (memberRepository.existsByEmail(signUpRequest.getEmail())) {
+            throw new EmailConflictException();
         }
 
-        if (memberRepository.existsByEmail(signUpRequest.getEmail())) {
-            throw new IllegalArgumentException("이미 등록된 이메일입니다.");
-        }
+        memberRepository.existsById(1L);
 
         String encodedPassword = passwordEncoder.encode(signUpRequest.getPassword());
+        log.info("[SignUp] encodedPassword = {}", encodedPassword);
 
         Member member = Member.builder()
                 .email(signUpRequest.getEmail())
@@ -33,7 +34,7 @@ public class MemberService {
                 .build();
 
         memberRepository.save(member);
-        log.info("[SignUp] 회원가입 완료");
+        log.info("[SignUp] end");
     }
 
 }
