772c26eb8c493e9921720943c04d13482664fb5a
오민성 <qww1552@naver.com>
2023-04-26 11:19:55 +0900

    feat: 주석 삭제

--- FE/moalarm/api/auth.js
+++ FE/moalarm/api/auth.js
@@ -10,13 +10,12 @@ onload = () => {
     const $signinForm = document.getElementById("signin-form");
     const $emailInput = document.getElementById("floatingInput");
     const $passwordInput = document.getElementById("floatingPassword");
-    console.log("onload");
+
     $signinForm.onsubmit = async (event)=>{
         event.preventDefault();
         try {
             const response = await signin($emailInput.value, $passwordInput.value);
             window.localStorage.setItem("token", response.token);
-            console.log("signin success");
             window.location.replace(`main-page.html`);
         } catch(error) {
             alert(error);
