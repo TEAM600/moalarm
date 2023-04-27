777e3e3b7922447f8bc4c4591045fb5fbd6f4c36
Seyoung Goo <0917sy@gmail.com>
2023-04-27 17:47:01 +0900

    feat: change exception message

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/member/exception/MoalarmKeyNotFoundException.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/member/exception/MoalarmKeyNotFoundException.java
@@ -5,6 +5,6 @@ import com.team600.moalarm.common.exception.NotFoundException;
 public class MoalarmKeyNotFoundException extends NotFoundException {
 
     public MoalarmKeyNotFoundException() {
-        super("해당 키와 일치하는 사용자가 없습니다.\n request header를 확인해주세요.\n Authorization: MoalarmKey {moalarm-key}");
+        super("Access Denied");
     }
 }
