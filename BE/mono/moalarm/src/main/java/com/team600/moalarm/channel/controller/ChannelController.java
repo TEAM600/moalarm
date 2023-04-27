1035cd0a7e09505001d7b22efe85aa1ce1cc9bc7
jehyuck <wpgur07@gmail.com>
2023-04-27 11:21:45 +0900

    feat: 유효성 검사 validation 어노테이션으로 변경

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/controller/ChannelController.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/controller/ChannelController.java
@@ -2,27 +2,29 @@ package com.team600.moalarm.channel.controller;
 
 import com.team600.moalarm.channel.data.dto.request.ChannelCreateRequest;
 import com.team600.moalarm.channel.data.dto.response.ChannelPossessionResponse;
-import com.team600.moalarm.channel.exception.ChannelTypeNotValidException;
 import com.team600.moalarm.channel.service.ChannelSaveService;
 import com.team600.moalarm.channel.service.ChannelService;
 import java.util.List;
 import java.util.Map;
+import javax.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@Validated
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/channels")
 public class ChannelController {
 
@@ -39,39 +41,25 @@ public class ChannelController {
 
     //TODO: login, repository, entity, service 작성 후 완성시키기
     @PostMapping("/type/{type}")
-    public ResponseEntity<Void> createChannel(@PathVariable("type") String type,
+    public ResponseEntity<Void> createChannel(
+            @PathVariable("type") @Pattern(regexp = "sms|mail|push", message = "올바르지 않은 타입값이 들어왔습니다.") String type,
             @RequestBody ChannelCreateRequest requestDto,
             @AuthenticationPrincipal UserDetails userDetails) {
         log.info("POST /channels/type/{}", type);
         String memberId = userDetails.getUsername();
-        switch (type) { //TODO: 채널별 DTO 유효성 검사
-            case "sms":
-            case "fcm":
-            case "mail":
-                channelSaveService.get(type + "ChannelSaveService")
-                        .saveChannel(requestDto, memberId);
-                break;
-            default:
-                throw new ChannelTypeNotValidException(type);
-        }
+        //TODO: 채널별 유효성 검사
+        channelSaveService.get(type + "ChannelService").saveChannel(requestDto, memberId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/type/{type}")
-    public ResponseEntity<Void> deleteChannel(@PathVariable("type") String type,
+    public ResponseEntity<Void> deleteChannel(
+            @PathVariable("type") @Pattern(regexp = "sms|mail|push", message = "올바르지 않은 타입값이 들어왔습니다.") String type,
             @AuthenticationPrincipal UserDetails userDetails) {
         log.info("delete /channels/type/{}", type);
         String memberId = userDetails.getUsername();
-        switch (type) {
-            case "sms":
-            case "fcm":
-            case "mail":
-                channelSaveService.get(type + "ChannelSaveService")
-                        .deleteChannel(type, memberId);
-                break;
-            default:
-                throw new ChannelTypeNotValidException(type);
-        }
+        channelSaveService.get(type + "ChannelService").deleteChannel(type, memberId);
+
         return ResponseEntity.noContent().build();
     }
 }
