dff9612a45d887809145cba8ea63a2ca1c2de376
Seyoung Goo <0917sy@gmail.com>
2023-04-28 16:43:41 +0900

    refactor: use @CurrentMemberId annotation to get memberId in KeyController

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/apikey/controller/KeyController.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/apikey/controller/KeyController.java
@@ -2,14 +2,13 @@ package com.team600.moalarm.apikey.controller;
 
 import com.team600.moalarm.apikey.dto.response.KeyResponse;
 import com.team600.moalarm.apikey.service.ApiKeyService;
+import com.team600.moalarm.common.annotation.CurrentMemberId;
 import java.net.URI;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,18 +24,15 @@ public class KeyController {
     private final ApiKeyService<?> apiKeyService;
 
     @GetMapping
-    public ResponseEntity<KeyResponse> getKey(
-            @AuthenticationPrincipal UserDetails userDetails) {
-        log.info("GET /key, {}", userDetails.getUsername());
-        long memberId = Long.parseLong(userDetails.getUsername());
+    public ResponseEntity<KeyResponse> getKey(@CurrentMemberId long memberId) {
+        log.info("GET /key, {}", memberId);
         return ResponseEntity.ok(apiKeyService.getApiKey(memberId));
     }
 
     @PostMapping
-    public ResponseEntity<KeyResponse> refreshKey(
-            @AuthenticationPrincipal UserDetails userDetails, HttpServletRequest request) {
-        log.info("POST /key, {}", userDetails.getUsername());
-        long memberId = Long.parseLong(userDetails.getUsername());
+    public ResponseEntity<KeyResponse> refreshKey(@CurrentMemberId long memberId,
+            HttpServletRequest request) {
+        log.info("POST /key, {}", memberId);
         return ResponseEntity.created(URI.create(request.getRequestURL().toString()))
                 .body(apiKeyService.refreshApiKey(memberId));
     }
