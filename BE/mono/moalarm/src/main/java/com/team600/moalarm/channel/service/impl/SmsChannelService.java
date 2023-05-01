f92163a70487d1949da8fe3c02a0bd5889184c87
jehyuck <wpgur07@gmail.com>
2023-05-01 15:48:47 +0900

    feat: 암호화 기능 추가

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/service/impl/SmsChannelService.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/service/impl/SmsChannelService.java
@@ -31,8 +31,13 @@ public class SmsChannelService implements ChannelSaveService {
             throw new ChannelConflictException("이미 채널을 소유중입니다.");
         }
 
-        Channel channel = Channel.builder().apiKey(requestDto.getKey()).memberId(member.getId())
-                .type(ChannelCode.SMS).secret(requestDto.getSecret()).build();
+        Channel channel = Channel.builder()
+                .apiKey(requestDto.getKey())
+                .memberId(member.getId())
+                .type(ChannelCode.SMS)
+                .secret(requestDto.getSecret())
+                .extraValue(requestDto.getExtraValue())
+                .build();
         member.registChannel(ChannelCode.SMS.ordinal());
         channelRepository.save(channel);
     }
