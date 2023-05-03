e5b75a0d29d8c3a11115a55842978b3c709eeedc
오민성 <qww1552@naver.com>
2023-05-03 20:08:55 +0900

    Feat: exception body 정의

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/common/exception/handler/GlobalExceptionHandler.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/common/exception/handler/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.team600.moalarm.common.exception.BadRequestException;
 import com.team600.moalarm.common.exception.ConflictException;
 import com.team600.moalarm.common.exception.NotFoundException;
 import javax.validation.ConstraintViolationException;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,22 +16,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<String> handleBadRequest(BadRequestException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ErrorBody> handleBadRequest(BadRequestException e) {
+        return ResponseEntity.badRequest().body(ErrorBody.of(e.getMessage()));
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handleNotFound(NotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorBody> handleNotFound(NotFoundException e) {
+        return new ResponseEntity<>(ErrorBody.of(e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handleConflict(ConflictException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+    public ResponseEntity<ErrorBody> handleConflict(ConflictException e) {
+        return new ResponseEntity<>(ErrorBody.of(e.getMessage()), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handleValidation(ConstraintViolationException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorBody> handleValidation(ConstraintViolationException e) {
+        return new ResponseEntity<>(ErrorBody.of(e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @Getter
+    static class ErrorBody {
+
+        private final String message;
+
+        private ErrorBody(String message) {
+            this.message = message;
+        }
+
+        static ErrorBody of(String message) {
+            return new ErrorBody(message);
+        }
     }
 }
