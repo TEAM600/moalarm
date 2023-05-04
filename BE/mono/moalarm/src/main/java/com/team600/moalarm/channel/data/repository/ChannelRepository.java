eed28c3a6455f7c72b73b0ad4b1369083b062122
jehyuck <wpgur07@gmail.com>
2023-05-04 11:25:40 +0900

    refactor: 분류 기준이 강한 조건을 우선으로 배치

--- BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/data/repository/ChannelRepository.java
+++ BE/mono/moalarm/src/main/java/com/team600/moalarm/channel/data/repository/ChannelRepository.java
@@ -9,10 +9,10 @@ import org.springframework.data.repository.query.Param;
 
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
 
-    @Query("SELECT c FROM Channel c WHERE c.delYn = 'N' AND c.memberId=:memberId")
+    @Query("SELECT c FROM Channel c WHERE c.memberId=:memberId AND c.delYn = 'N'")
     List<Channel> findAllByMemberId(long memberId);
 
-    @Query("SELECT c FROM Channel c WHERE c.delYn = 'N' AND c.memberId=:memberId AND c.type = :type")
+    @Query("SELECT c FROM Channel c WHERE c.memberId=:memberId AND c.type = :type AND c.delYn = 'N'")
     Channel findAllByMemberIdAndType(@Param("type") ChannelCode channelCode, long memberId);
 
     @Query("SELECT COUNT(*) > 0 "
