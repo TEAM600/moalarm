34f5a802ebaf89ad11cd30d59f423a01178f745f
오민성 <qww1552@naver.com>
2023-05-12 14:34:52 +0900

    feat: test repository 삭제

--- auth/src/main/java/com/team600/moalarm/auth/data/repository/MemberRepository.java
+++ auth/src/main/java/com/team600/moalarm/auth/data/repository/MemberRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
 }
